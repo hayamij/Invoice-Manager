ebfd1190ad017a6e7e9a7350afee773dde7b4e42
Nguyen Quang Tuan Phuong <130271080+hayamij@users.noreply.github.com>
2025-08-19 15:05:45 +0700

    complete refresh button

--- src/main/java/presentation/Controller/RefreshInvoiceController.java
+++ src/main/java/presentation/Controller/RefreshInvoiceController.java
@@ -1,32 +1,18 @@
 package presentation.Controller;
 
-import business.Controls.ShowInvoiceList.ShowInvoiceListUseCase;
-import persistence.InvoiceList.InvoiceDAO;
-import persistence.InvoiceList.InvoiceDAOGateway;
-import presentation.Model.InvoiceViewModel;
-
 public class RefreshInvoiceController {
-
     private ShowInvoiceListController showInvoiceListController;
-    private InvoiceDAOGateway invoiceDAOGateway;
-    private InvoiceViewModel invoiceViewModel;
-    private ShowInvoiceListUseCase showInvoiceListUseCase;
 
     public RefreshInvoiceController(ShowInvoiceListController controller) {
         this.showInvoiceListController = controller;
     }
 
     public void refreshInvoice() {
-        invoiceViewModel = new InvoiceViewModel();
-        invoiceDAOGateway = new InvoiceDAO();
-        showInvoiceListUseCase = new ShowInvoiceListUseCase(invoiceDAOGateway);
-        showInvoiceListController = new ShowInvoiceListController(invoiceViewModel, showInvoiceListUseCase);
         if (showInvoiceListController != null) {
             showInvoiceListController.execute();
-            System.out.println("ShowInvoiceListController executed to refresh invoice list.");
-            invoiceViewModel.notifySubscribers(); // Notify view to update
+            System.out.println("RefreshInvoiceController: Executed refresh successfully.");
         } else {
-            System.out.println("ShowInvoiceListController is not set.");
+            System.out.println("RefreshInvoiceController: ShowInvoiceListController is null!");
         }
     }
 }
